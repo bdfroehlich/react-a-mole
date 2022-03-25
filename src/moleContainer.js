bbef573c4932f0ec0b91f8d8b0e12bab63302e79
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-03-25 13:25:24 -0500

    adding images, and mole / moleContainer components

---
+++ src/moleContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Mole from './mole'
+
+function MoleContainer(props) {
+    return (
+        <div>
+            <h2> Mole Container </h2>
+            <Mole />
+        </div>
+    )
+}
+
+export default MoleContainer
